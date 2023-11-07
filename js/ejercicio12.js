51544dba7860f36a10da1d08767041825f09e52d
SebastianVillagra <sebastianvilla16.com@gmail.com>
2023-11-07 14:35:54 -0300

    ejercicio 12 terminado

---
+++ js/ejercicio12.js
@@ -0,0 +1,5 @@
+// Generar un número aleatorio entre 1 y 99
+
+const numeroAleatorio = Math.floor(Math.random() * 99) + 1;  //le sumo 1 pq sino me daba del 1 al 98
+
+console.log("Número aleatorio entre 1 y 99 elegido --> ", numeroAleatorio);
